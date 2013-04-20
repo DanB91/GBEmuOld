4add62f31889241840faa41b532b88ce3961079e
Dan <dan.bokser@gmail.com>
2013-04-20 02:42:27 -0400

    added MMU class def and stub implementation.

--- src/MMU.cpp
+++ src/MMU.cpp
@@ -1,1 +1,17 @@
 #include "MMU.h"
+
+unsigned char MMU::readByte(unsigned short address){
+    return 0;
+}
+
+
+void MMU::writeByte(unsigned char value, unsigned short address){
+}
+
+
+unsigned short MMU::readWord(unsigned short address){
+    return 0;
+}
+
+void MMU::writeWord(unsigned short value, unsigned short address){
+}
