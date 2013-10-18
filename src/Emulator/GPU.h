5c3462c2d3b2563dd492f988825117d057bae3a0
Dan <dan.bokser@gmail.com>
2013-10-17 23:39:24 -0400

    Part of the commit 2 commits ago

--- src/Emulator/GPU.h
+++ src/Emulator/GPU.h
@@ -6,14 +6,11 @@ class GPU
 public:
     GPU();
 
-<<<<<<< HEAD
     /**
      * @brief step steps the GPU
      * @param numCycles the number of cycles the CPU has done since last instruction
      * @return true if should draw to screen, else false
      */
-=======
->>>>>>> a3a03e973497b104abfc443a1a45aff163377c6a
     bool step(int numCycles);
 
 private:
